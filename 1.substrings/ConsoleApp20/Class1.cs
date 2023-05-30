a7f7d2e56d0462e765a11ef205d8f1377c373081
krishna <sravanikashetti@gmail.com>
2023-05-30 10:41:09 +0530

    substring is pushed

---
+++ 1.substrings/ConsoleApp20/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    class TestCases
+    {
+        public void testcaseshandle()
+        {
+            Console.WriteLine("Input should'nt be: ");
+            Console.WriteLine("empty string");
+            Console.WriteLine("empty substring");
+            Console.WriteLine("empty string and substring");
+        }
+    }
+}
---
+++ 1.substrings/ConsoleApp20/Class2.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp20
+{
+     class Validations
+    {
+        public static int TestcasesValidation(string givenString,string givenSubString)
+        {
+            int result;
+            if (givenString.Length == 0 || givenSubString.Length == 0 || givenString.Length == 0 && givenSubString.Length == 0)
+            {
+                result = 0;
+            }
+            else
+            {
+                result = 1;
+            }
+            return result;
+        }
+    }
+}
