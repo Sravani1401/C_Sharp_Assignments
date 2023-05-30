bb3b3e50e62d2bbaefe67c0d18fb710db9972b4a
krishna <sravanikashetti@gmail.com>
2023-05-30 11:24:35 +0530

    file

---
+++ one.substrings/ExceptionHandling.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Substrings
+{
+    /// <summary>
+    /// clss to print the validation message
+    /// </summary>
+    class ExceptionHandling
+    {
+        /// <summary>
+        /// Method to kprint the validation message
+        /// </summary>
+        /// <param name="result"></param>
+        public void Display(string result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
