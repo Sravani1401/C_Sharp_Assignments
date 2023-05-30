b844ba4aedc21f06c9f8d35c620624e949265a2f
krishna <sravanikashetti@gmail.com>
2023-05-30 10:43:47 +0530

    file pushed

---
+++ 2.MaximumProduct/ConsoleApp21/FunctionToFindMaxProduct.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumProductofAdjacentdigits
+{
+    /// <summary>
+    /// Class Function to find maximum product for the Adjacent digits
+    /// </summary>
+     class FunctionToFindMaxProduct
+    {
+        /// <summary>
+        /// Function to find maximum product for the adjacent digits
+        /// </summary>
+        /// <param name="givenValue"></param>
+        /// <returns></returns>
+        public List<int> MaxProductOfAdjacentDigits(string givenInput)
+        {
+            List<int> List1 = new List<int>();
+
+            int product, maxProduct = 1;
+            string findResult = "";
+
+            for (int i = 0; i <= givenInput.Length - 4; i++)
+            {
+
+                char a = givenInput[i];
+                char b = givenInput[i + 1];
+                char c = givenInput[i + 2];
+                char d = givenInput[i + 3];
+                string result = a.ToString() + b.ToString() + c.ToString() + d.ToString();
+                product = (a - '0') * (b - '0') * (c - '0') * (d - '0');
+                /*Console.WriteLine(Product);*/
+                if (maxProduct < product)
+                {
+                    maxProduct = product;
+                    findResult = (result);
+                }
+
+            }
+            List1.Add(Convert.ToInt32(findResult));
+            List1.Add(maxProduct);
+            return List1;
+        }
+       
+    }
+}
---
+++ 2.MaximumProduct/ConsoleApp21/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumProductofAdjacentdigits
+{
+
+
+
+    /// <summary>
+    /// class Find the four adjacent digits in any given number that have the greatest product. What is the value of this product
+    /// </summary>
+     class ToFindMaximumProduct
+    {
+        /// <summary>
+        /// Method to find the four adjacent digits in any given number that have the greatest product. What is the value of this product
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main()
+        {
+            string givenInput = (Console.ReadLine());
+            ValidationsForMaxProduct o = new ValidationsForMaxProduct();
+            bool isValid = o.TestcasesValidations((givenInput));
+            if (isValid == false) return;
+            FunctionToFindMaxProduct object1 = new FunctionToFindMaxProduct();
+
+            List<int> List1 = (object1.MaxProductOfAdjacentDigits((givenInput)));
+            Displayoflist object2 = new Displayoflist();
+            object2.DisplayMethod(List1);
+        }
+    }
+}
