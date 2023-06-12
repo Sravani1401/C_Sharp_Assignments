945a84f9a63e46319d61067c9dc3b125867f5328
krishna <sravanikashetti@gmail.com>
2023-06-12 20:20:12 +0530

    ThreeTierExample

---
+++ MaxProduct/ConsoleApp21/ConsoleApp21/handling.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace ConsoleApp21
+{
+    /// <summary>
+    /// To print the Input validation message
+    /// </summary>
+    class HandlingCases
+    {
+        public void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
